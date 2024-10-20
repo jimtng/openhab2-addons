0d41d8fcfc32fa79ed0cb3c982d61e8e02b955b4
Dan Cunningham <dan@digitaldan.com>
2024-10-20 13:25:19 -0700

    Forgot to add the new Action class
    Signed-off-by: Dan Cunningham <dan@digitaldan.com>

---
+++ bundles/org.openhab.binding.matter/src/main/java/org/openhab/binding/matter/internal/actions/MatterEndpointActions.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) 2010-2024 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.matter.internal.actions;
+
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.openhab.binding.matter.internal.client.MatterWebsocketClient;
+import org.openhab.binding.matter.internal.client.model.PairingCodes;
+import org.openhab.binding.matter.internal.handler.EndpointHandler;
+import org.openhab.core.automation.annotation.ActionOutput;
+import org.openhab.core.automation.annotation.RuleAction;
+import org.openhab.core.thing.binding.ThingActions;
+import org.openhab.core.thing.binding.ThingActionsScope;
+import org.openhab.core.thing.binding.ThingHandler;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ServiceScope;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@NonNullByDefault
+/**
+ * @author Dan Cunningham
+ */
+@Component(scope = ServiceScope.PROTOTYPE, service = MatterEndpointActions.class)
+@ThingActionsScope(name = "matter")
+public class MatterEndpointActions implements ThingActions {
+    public final Logger logger = LoggerFactory.getLogger(getClass());
+    private @Nullable EndpointHandler handler;
+
+    @Override
+    public void setThingHandler(@Nullable ThingHandler handler) {
+        this.handler = (EndpointHandler) handler;
+    }
+
+    @Override
+    public @Nullable ThingHandler getThingHandler() {
+        return handler;
+    }
+
+    @RuleAction(label = "generate new pairing codes for device", description = "Generates a new manual and QR pairing code to be used to pair the device with an external Matter controller")
+    public @ActionOutput(name = "result", label = "New manual and QR pairing codes", type = "java.util.Map") Map<String, String> generateNewPairingCode() {
+        EndpointHandler handler = this.handler;
+        if (handler != null) {
+            MatterWebsocketClient client = handler.getClient();
+            if (client != null) {
+                try {
+                    PairingCodes code = client.enhancedCommissioningWindow(handler.getNodeId()).get();
+                    return Map.of("manualPairingCode", code.manualPairingCode, "qrPairingCode", code.qrPairingCode);
+                } catch (InterruptedException | ExecutionException e) {
+                    logger.debug("Failed to generate new pairing code for device {}", handler.getNodeId(), e);
+                }
+            }
+        }
+        return Map.of();
+    }
+}
